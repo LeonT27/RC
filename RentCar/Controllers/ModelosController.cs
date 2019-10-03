a06cf4265237c123bc8f920aaa6fc2650318fc0e
ltolentino <ltolentino@cdeee.gob.do>
2019-10-03 10:16:22 -0400

    Agregando modelos

---
+++ RentCar/Controllers/ModelosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RentCar.Models;
+
+namespace RentCar.Controllers
+{
+    public class ModelosController : Controller
+    {
+        private RC db = new RC();
+
+        // GET: Modelos
+        public ActionResult Index()
+        {
+            var modelo = db.Modelo.Include(m => m.Marca);
+            return View(modelo.ToList());
+        }
+
+        // GET: Modelos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Modelo modelo = db.Modelo.Find(id);
+            if (modelo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(modelo);
+        }
+
+        // GET: Modelos/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdMarca = new SelectList(db.Marca, "Id", "Descripcion");
+            return View();
+        }
+
+        // POST: Modelos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,IdMarca,Descripcion,Estado")] Modelo modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Modelo.Add(modelo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdMarca = new SelectList(db.Marca, "Id", "Descripcion", modelo.IdMarca);
+            return View(modelo);
+        }
+
+        // GET: Modelos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Modelo modelo = db.Modelo.Find(id);
+            if (modelo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdMarca = new SelectList(db.Marca, "Id", "Descripcion", modelo.IdMarca);
+            return View(modelo);
+        }
+
+        // POST: Modelos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,IdMarca,Descripcion,Estado")] Modelo modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(modelo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdMarca = new SelectList(db.Marca, "Id", "Descripcion", modelo.IdMarca);
+            return View(modelo);
+        }
+
+        // GET: Modelos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Modelo modelo = db.Modelo.Find(id);
+            if (modelo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(modelo);
+        }
+
+        // POST: Modelos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Modelo modelo = db.Modelo.Find(id);
+            db.Modelo.Remove(modelo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
